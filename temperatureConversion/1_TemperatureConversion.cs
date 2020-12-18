94afce4bd0073f6f38aed5f124c2252989d43e83
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:12:45 -0500

    temperatureConversion exercise

---
+++ temperatureConversion/1_TemperatureConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace _1_TemperatureConversion
+{
+    class _1_TemperatureConversion
+    {
+        static void Main(string[] args)
+        {
+            double celsius = 0;
+            double fahrenheit;
+
+            fahrenheit = (9.0 / 5.0 * celsius) + 32.0;
+
+            WriteLine("\t Temperature Conversion App");
+            WriteLine("\tDegrees in Celsius:{0,8}", celsius);
+            WriteLine("\tDegrees in Fahrenheit:{0,7:f1}", fahrenheit);
+            ReadKey();
+        }
+    }
+}
