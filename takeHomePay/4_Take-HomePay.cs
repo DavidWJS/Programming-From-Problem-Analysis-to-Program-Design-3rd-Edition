e82f5391631f31ef5123ce2eea4dd5b5bc78bacc
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:14:53 -0500

    takeHomePay exercise

---
+++ takeHomePay/4_Take-HomePay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace _4_Take_HomePay
+{
+    class _4_Take_HomePay
+    {
+        static void Main(string[] args)
+        {
+            int monthlySales = 161432;
+            string employeeName = "Joshua Montain";
+
+            Double grossPay = monthlySales * .07;
+            Double federalTax = grossPay * .18;
+            Double retirement = grossPay * .1;
+            Double socialSecurity = grossPay * .06;
+            Double totalDeductions = socialSecurity + retirement + federalTax;
+            Double takeHomePay = grossPay - totalDeductions;
+
+            Write(employeeName + "'s Monthly Sale amount is: {0,14:c}", monthlySales);
+            Write("\nGross Pay: {0,44:c}", grossPay);
+            Write("\nFed Tax: {0,46:c}", federalTax);
+            Write("\nRetirement: {0,43:c}", retirement);
+            Write("\nSocial Security: {0,38:c}", socialSecurity);
+            Write("\nTotal Deductions: {0,37:c}", totalDeductions);
+            Write("\nMaking " + employeeName + "'s take home pay: {0,16:c}", takeHomePay);
+            Write("\n");
+            ReadKey();
+        }
+    }
+}
