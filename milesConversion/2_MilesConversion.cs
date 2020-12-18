a33bcb0ee9ca99a9b77b08903b662b54df9b4b81
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:13:40 -0500

    milesConversion exercise

---
+++ milesConversion/2_MilesConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace _2_MilesConversion
+{
+    class _2_MilesConversion
+    {
+        static void Main(string[] args)
+        {
+            double miles = 4.5;
+            double feet;
+            double kilometers;
+
+            feet = 5280 * miles;
+            kilometers = feet * 0.0003048;
+            WriteLine("{0:f2}", miles + " mi");
+            WriteLine("{0} mi = {1:f0} ft", miles, feet);
+            WriteLine("{0} ft = {1:f0} km", feet, kilometers);
+            ReadKey();
+        }
+    }
+}
