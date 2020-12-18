362b906153efa6521e6655a4a1a48c869bd98495
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:14:09 -0500

    changeReturn exercise

---
+++ changeReturn/3_ChangeReturn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace _3_ChangeReturn
+{
+    class _3_ChangeReturn
+    {
+        static void Main(string[] args)
+        {
+            const int change = 92;
+            int quarters;
+            int dimes;
+            int nickels;
+            int pennies;
+           
+            quarters = change / 25;
+            dimes = (change % 25) / 10;
+            nickels = ((change % 25) % 10) / 5;
+            pennies = (((change % 25) % 10) % 5);
+
+            WriteLine("Your change is: {0,-8:f0}", "." + change);
+            WriteLine("Quarters:{0,3}", quarters);
+            WriteLine("Dimes:{0,6}", dimes);
+            WriteLine("Nickels:{0,4}", nickels);
+            WriteLine("Pennies:{0,4}", pennies);
+            ReadKey();
+        }
+    }
+}
